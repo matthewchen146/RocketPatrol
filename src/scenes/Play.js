26ef6cfbe9edb3533eccb9ed050327de5bb182b5
Mattowskinator <mchen146@ucsc.edu>
2022-04-07 19:40:46 -0700

    added rocket to play scene

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -26,7 +26,8 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
 
-        
+        // add rocket (p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
     }
 
     update() {
