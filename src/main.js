db91994dd6def136dccd1a97cd1f9b6d9d57a169
Mattowskinator <mchen146@ucsc.edu>
2022-04-07 19:48:46 -0700

    added rocket ship movement and firing

--- src/main.js
+++ src/main.js
@@ -12,3 +12,6 @@ const game = new Phaser.Game(config);
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
