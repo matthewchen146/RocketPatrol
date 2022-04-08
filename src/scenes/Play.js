d9b9c7f5b912af701f2723be1b6a9d5112752464
Mattowskinator <mchen146@ucsc.edu>
2022-04-07 19:37:31 -0700

    added moving starfield

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -13,7 +13,10 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-        // this.add.text(20, 20, "Rocket Patrol Play");
+
+
+        // place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
 
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
@@ -22,5 +25,11 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+
+        
+    }
+
+    update() {
+        this.starfield.tilePositionX -= 4;
     }
 }
